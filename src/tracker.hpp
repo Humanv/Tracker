e98fcbc44bbbbbe77ba467c6184cefd447d4c77a
Humanv <sunhao321323@163.com>
2019-07-31 17:46:31 +0800

    updata description

--- src/tracker.hpp
+++ src/tracker.hpp
@@ -1,9 +1,3 @@
-/*******************************************************************************
-* Created by Qiang Wang on 16/7.24
-* Copyright 2016 Qiang Wang.  [wangqiang2015-at-ia.ac.cn]
-* Licensed under the Simplified BSD License
-*******************************************************************************/
-
 #ifndef SRC_Tracker_HPP_
 #define SRC_Tracker_HPP_
 
